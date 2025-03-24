f7a99b37749bf7a45d49468eec64f89633d4d2fc
Ozod Hamzayev <ozodhamzayev10@gmail.com>
2025-03-24 13:03:15 +0500

    lesson

--- app/Services/Admin/ModeratorServices.php
+++ app/Services/Admin/ModeratorServices.php
@@ -25,6 +25,7 @@ class ModeratorServices
     public function moderatorStore($validated): RedirectResponse
     {
         $validated += ['role' => "2"];
+        $asd = 12;  
         $validated['password'] = bcrypt($validated['password']);
         User::query()->create($validated);
         return redirect()->route('admin.moderators.index');
